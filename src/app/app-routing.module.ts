8566753db268a235d18937710fcf05eb5373d653
diegogm90 <diegogm90@gmail.com>
2022-03-02 22:06:49 +0100

    pequeño update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponentComponent } from './components/blog-component/blog-component.component';
 import { FormComponent } from './components/form/form.component';
+import { ViewPostComponent } from './components/view-post/view-post.component';
 
 const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: '/blog-component' },
   { path: "blog-component", component: BlogComponentComponent },
   { path: "new", component: FormComponent },
+  { path: "post/:idpost", component: ViewPostComponent },
   { path: "**", redirectTo: '/blog-component' },
 ];
 
